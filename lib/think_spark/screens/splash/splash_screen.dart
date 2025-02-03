7f13a60887a8668caaa0cca0eb83190e7bc8efe5
karim <karimslama917@gmail.com>
2025-02-04 00:51:54 +0200

    - Design and Finish Splash Screen to display for 4 sec then it navigates to another screen. - Define the route for Splash. - Consist Themes Mode for the app dark and light. - Define separate class for Strings and another for Colors. - Use Assets Generator package to auto generate the Assets used for the App.

---
+++ lib/think_spark/screens/splash/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:think_spark/core/helpers/extensions.dart';
+import 'package:think_spark/core/routing/routes.dart';
+import 'package:think_spark/core/theming/app_colors/app_colors.dart';
+import 'package:think_spark/core/theming/app_strings/app_string.dart';
+import 'package:think_spark/gen/assets.gen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: AppColors.linear,
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Column(
+                spacing: 10.h,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                      width: 220.w,
+                      image: AssetImage(Assets.images.lightBulbPng.path)),
+                  SvgPicture.asset(Assets.images.thinkSparkSvg, width: 260.w),
+                  Text(AppString.theSocietyOfNewIdeas),
+                ],
+              ),
+              SvgPicture.asset(Assets.images.circleBg),
+            ],
+          )),
+    );
+  }
+
+  Future navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 4), () {
+      context.pushNamed(
+        Routes.onBoardingScreen,
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+}
